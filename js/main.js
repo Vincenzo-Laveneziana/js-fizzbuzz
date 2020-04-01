6e5e7cc8a95a95f7ee6ed45f6bf0f97b1da61096
Vincenzo <laveneziana.vincenzo@gmail.com>
2020-04-01 18:14:01 +0200

    Creazione mini player youtube

--- js/main.js
+++ js/main.js
@@ -11,34 +11,50 @@ var creaNumeri =0;
 var list = document.getElementById("list");
 var elementi ="";
 var n = 0;
-//console.log("Ecco a voi una delle più belle note musicali")
-for( var i = 0; i < 100 ; i++){
-  n++;
-  creaNumeri = i+ 1;
 
-  if ((creaNumeri % 3 == 0) && (creaNumeri % 5 != 0)){
+bottoneSottotitoli.addEventListener( "click", function(){
+  containerLi.className = "show";
+  bottoneSottotitoli.className = "hidden";
+  bottoneSottotitoli2.className = "show";
+ 
 
-    creaNumeri = "BOBOBOBOBO";
-    console.log("Fizz      "+ n);
-
-  } else if ((creaNumeri % 5 == 0) && (creaNumeri % 3 != 0)){
-   
-    creaNumeri = "POPOPOPOPO";
-    console.log("Buzz      " + n );
-
-    //i multipli di 5
-  } else if ((creaNumeri % 3 == 0) && (creaNumeri % 5 == 0)){
-
-    creaNumeri = "EEEEMMMMBBBB";
-    console.log("FizzBuzz  " + n);
-    // i numeri che sono sia multipli di 3 che di 5
-  } else {
-    creaNumeri = "LALALALALALA";
-    console.log("Rimanenti " + n);
-    //i rimanenti
-  }
-
-  elementi +="<li>" + creaNumeri + "</li>";
+  for( var i = 0; i < 100 ; i++){
+    n++;
+    creaNumeri = i+ 1;
   
-}//fine ciclo for
-list.innerHTML= elementi;
+    if ((creaNumeri % 3 == 0) && (creaNumeri % 5 != 0)){
+  
+      creaNumeri = "BOBOBOBOBO";
+      console.log("Fizz      "+ n);
+  
+    } else if ((creaNumeri % 5 == 0) && (creaNumeri % 3 != 0)){
+     
+      creaNumeri = "POPOPOPOPO";
+      console.log("Buzz      " + n );
+  
+      //i multipli di 5
+    } else if ((creaNumeri % 3 == 0) && (creaNumeri % 5 == 0)){
+  
+      creaNumeri = "EEEEMMMMBBBB";
+      console.log("FizzBuzz  " + n);
+      // i numeri che sono sia multipli di 3 che di 5
+    } else {
+      creaNumeri = "LALALALALALA";
+      console.log("Rimanenti " + n);
+      //i rimanenti
+    }
+  
+    elementi +="<li>" + creaNumeri + "</li>";
+    
+  }//fine ciclo for
+  list.innerHTML= elementi;
+
+ }
+);
+
+bottoneSottotitoli2.addEventListener( "click", function(){
+  bottoneSottotitoli2.className = "hidden";
+  bottoneSottotitoli.className = "show";
+  containerLi.className = "hidden";
+ }
+);
